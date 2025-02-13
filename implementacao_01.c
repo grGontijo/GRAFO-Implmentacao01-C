176c7adbbd0936b52b03dbfd6fba1f2e9646aed5
grGontijo <gabrielgontijocc@gmail.com>
2025-02-13 15:39:47 -0300

    procurando errro

--- implementacao_01.c
+++ implementacao_01.c
@@ -4,23 +4,36 @@
 #include <stdlib.h>
 #include <string.h>
 void getMetadadosGrafo(FILE *ptr, int *n, int *m);
-
+void lerArestas(FILE *ptr, int *aresta);
 int main()
 {
     FILE *file = fopen("teste.txt", "r");
     int num_vertice;
     int num_aresta;
-    
-    if(!file){
+
+    if (!file)
+    {
         printf("Erro ao abrir o arquivo");
     }
     getMetadadosGrafo(file, &num_vertice, &num_aresta);
-    int *vertice = (int *)calloc(num_vertice, sizeof(int));
-    
+    int *aresta = (int *)calloc(num_aresta * 2, sizeof(int));
+    lerArestas(file, aresta);
+    puts("concluiu lida de arestas");
+    fclose(file);
     return 0;
 }
 
 void getMetadadosGrafo(FILE *ptr, int *n, int *m)
 {
     fscanf(ptr, "%d %d", n, m);
+}
+
+void lerArestas(FILE *ptr, int *aresta)
+{
+    int i = 0;
+    while (fscanf(ptr, "%d %d", aresta[i], aresta[i + 1]) == 2)
+    {
+        printf("Aresta: %d - %d", aresta[i], aresta[i + 1]);
+        i += 2;
+    }
 }
